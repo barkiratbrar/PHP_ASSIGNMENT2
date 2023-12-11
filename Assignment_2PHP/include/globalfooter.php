703d87d605bccba50aa6583c8def4d132b22ec8c
Barkirat Singh Brar <138818899+barkiratbrar@users.noreply.github.com>
2023-12-11 11:31:40 -0500

    Add files via upload

---
+++ Assignment_2PHP/include/globalfooter.php
@@ -0,0 +1,4 @@
+<link rel = "stylesheet" href="css/style.css" type="text/css">
+
+
+<p>@2023 MY PORTAL All rights reserved.</p>
---
+++ Assignment_2PHP/include/globalheader.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="../css/style.css" type="text/css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" >
+  	<link rel="stylesheet" href="css/style.css">
+	
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" ></script>
+<style>
+  .custom-navbar{
+    background-color:#ffae00;
+  }
+</style>
+
+<nav class="navbar navbar-dark custom-navbar">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php"><img src="https://img.freepik.com/free-vector/modern-resume-template_23-2147836674.jpg?w=996&t=st=1702311174~exp=1702311774~hmac=277c9109df4f7df4eb4888fe8f4b6ea44777fca5cff2393de6fc4ab1533278ce" alt="header logo" style="border-radius:20px"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="display-person.php">View</a></li>
+                    <li class="nav-item"><a class="nav-link" href="signin.php">Sign In</a></li>
+                    <li class="nav-item"><a class="nav-link" href="addimage.php">ADD Images</a></li>
+                    <li class="nav-item"><a class="nav-link" href="viewimage.php">View Images</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
